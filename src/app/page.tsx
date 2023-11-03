5cde0f665cde473e272a10481c667082ad4888d0
Raman Temre <rtemre@systango.com>
2023-11-03 20:09:47 +0530

    Create login signup & header UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { Connect } from "@/components/Connect";
 import { Connected } from "@/components/Connected";
+import { Home } from "@/components/Home";
+import Login from "@/components/Login";
+import { Signup } from "@/components/Signup";
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
-      <div>Home Page</div>
-      <hr />
+      <Home />
       <h2>Connect to wallet</h2>
       <Connect />
       <Connected>
         <h2>Connection Esatblished</h2>
       </Connected>
+      <hr />
+      <Login />
+      {/* <hr />
+      <Signup /> */}
     </div>
   );
 }
