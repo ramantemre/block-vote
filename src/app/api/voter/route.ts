fc3c0683f6b81530a184589cc8a6fafaf34b986e
Raman Temre <rtemre@systango.com>
2023-11-22 18:06:26 +0530

    Remove unused file

--- src/app/api/voter/route.ts
+++ src/app/api/voter/route.ts
@@ -1,5 +1,5 @@
 // import { }from '@/models/voter'
-import clientPromise from "../../../../lib/mongodb";
+import clientPromise from "@/lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import VoterModel from "@/models/voter";
